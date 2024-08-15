08c7c1f6c3db5d854fb2d0643b2e73c17feb9e7f
emsim11 <emsimone11@gmail.com>
2024-08-15 18:43:15 -0400

    Update Logo Paths

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-9637eeee'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.p7l4226510g"
+    "revision": "0.5mdoorm3jk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
